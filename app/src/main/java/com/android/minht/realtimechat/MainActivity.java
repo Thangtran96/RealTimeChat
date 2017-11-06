49f19d6a6102b98fc4fd9400cf32d79d499d0c2c
Thangtran96 <minhthang.tran.398.15@gmail.com>
2017-11-07 00:03:33 +0700

    Initial commit

---
+++ app/src/main/java/com/android/minht/realtimechat/MainActivity.java
@@ -0,0 +1,17 @@
+package com.android.minht.realtimechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void comitTest(){
+        int a=2;
+    }
+}
