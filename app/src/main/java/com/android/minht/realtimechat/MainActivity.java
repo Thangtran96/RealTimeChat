e51f857197a55804416118c35ee961b64abaed63
Thangtran96 <minhthang.tran.398.15@gmail.com>
2017-11-08 12:06:53 +0700

    create layout messsage, test button

--- app/src/main/java/com/android/minht/realtimechat/MainActivity.java
+++ app/src/main/java/com/android/minht/realtimechat/MainActivity.java
@@ -2,16 +2,31 @@ package com.android.minht.realtimechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btSend;
+    EditText etInput;
+    ListView lvChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etInput = (EditText) findViewById(R.id.etInput);
+        lvChat = (ListView) findViewById(R.id.lvChat);
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etInput.setText("");
+            }
+        });
     }
 
-    private void comitTest(){
-        int a=2;
-    }
+
 }
