63913bd6b44121b394ca173a4ba35cb765e02597
Thangtran96 <minhthang.tran.398.15@gmail.com>
2017-11-09 01:18:03 +0700

    setup firebase real time database and test it

--- app/src/main/java/com/android/minht/realtimechat/MainActivity.java
+++ app/src/main/java/com/android/minht/realtimechat/MainActivity.java
@@ -2,21 +2,34 @@ package com.android.minht.realtimechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btSend;
     EditText etInput;
     ListView lvChat;
+    private static final String TAG = "RealTimeChatApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, World!");
 
         etInput = (EditText) findViewById(R.id.etInput);
         lvChat = (ListView) findViewById(R.id.lvChat);
@@ -27,6 +40,24 @@ public class MainActivity extends AppCompatActivity {
                 etInput.setText("");
             }
         });
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
 
 
